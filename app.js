f71a6e751d2427bc5bd08fca16915d8b76458b4f
ItsMeEC <evan.ciavaglia3@gmail.com>
2017-03-01 12:37:09 -0500

    js

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+
+
+
+
 $(document).ready(function() {
     $("#datepicker").datepicker();
   });
+
+function loadImage(date) {
+    var params = {
+        api_key : key,
+        hd: true
+    };
+
+    if (date != null) {
+        params['date'] = date;
+    }
+
+    var request = $.ajax({
+        url: ENDPOINT,
+        data: params,
+    });
+
+    request.done(function( data ) {
+        $('#img').attr('src', data.hdurl);
+        $('#title').html(data.title);
+        $('#description').html(data.explanation);
+    });
+
+
