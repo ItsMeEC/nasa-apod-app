62360d5d3db3f36f2baea470ea1e191c47b1b52d
ItsMeEC <evan.ciavaglia3@gmail.com>
2017-03-07 19:43:19 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,13 @@
 
+/*window.setInterval(function() {
+$('.blinkText').toggle();
+}, 300);*/
 
-
-/*
 $(document).ready(function() {
     $("#datepicker").datepicker();
   });
 
-$(document).ready(function() {
-  $(".prev").click();
-});
-
-$(document).ready(function() {
-  $(".next").click();
-});
-
-var request = $.ajax({
-        url: 'https://api.nasa.gov/planetary/apod',
-        data: params,
-});
-
-request.done(function( data ) {
-    $('#img').attr('src', data.hdurl); 
-    $('#title').html(data.title);
-    $('#description').html(data.explanation);
-});
-
-var PAGE_ELEMENTS = {
- 'start': $('.start-page'),
- 'main': $('.main-page'),
-};
-
-$("form[name='app-start']").submit(function(event) {
-  event.preventDefault();
-  setRoute(state, 'main');
-  renderApp(state, PAGE_ELEMENTS);
-});
-*/
-
-
-
-//ajax call
+  
 var api_key = 'ri0EZkle2DPanZWtyS8oZFgC4coub86CDtV5OiYV';
 
 var nasa_url = 'https://api.nasa.gov/planetary/apod';
@@ -65,7 +33,7 @@ var imageLink = data.url;
 var heading = data.title;
 var descp =data.explanation;
 var date= data.date;
-var elem = '<h2> '+ heading +' </h2><div id="picture-div"><img class="img-responsive" src="'+ imageLink +'" /></a></div><div id="text-div"><p> '+ descp +' </p></div>';
+var elem = '<h2 style= "color: white"> '+ heading +' </h2><div id="picture-div"><img style= "height: 400px; width: 400px;" class="img-responsive nasa-img" src="'+ imageLink +'" /></a></div><div id="text-div"><p style= "color: white" > '+ descp +' </p></div>';
 results.append(elem);
 }
 
