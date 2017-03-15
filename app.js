2a1a2e952d1a7c218fa37c63f8fb43778550a415
ItsMeEC <evan.ciavaglia3@gmail.com>
2017-03-15 10:18:58 -0400

    datepicker

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ $('.blinkText').toggle();
     $("#datepicker").datepicker();
   }); */
 
-  
+$(function() {
+     $( "#datepicker" ).datepicker({ dateFormat: 'yy-mm-dd'}); 
+});  
+
 var api_key = 'ri0EZkle2DPanZWtyS8oZFgC4coub86CDtV5OiYV';
 
 var nasa_url = 'https://api.nasa.gov/planetary/apod';
@@ -33,7 +36,7 @@ var imageLink = data.url;
 var heading = data.title;
 var descp =data.explanation;
 var date= data.date;
-var elem = '<h2 style= "color: white"> '+ heading +' </h2><div id="picture-div"><img style= "height: 400px; width: 400px;" class="img-responsive nasa-img" src="'+ imageLink +'" /></a></div><div id="text-div"><p style= "color: white" > '+ descp +' </p></div>';
+var elem = '<h2 style= "color: white; padding-top: 50px;"> '+ heading +' </h2><div id="picture-div"><img style= "height: 400px; width: 400px; margin-top: 50px; margin-bottom: 50px;" class="img-responsive nasa-img" src="'+ imageLink +'" /></a></div><div id="text-div"><p style= "color: white" > '+ descp +' </p></div>';
 results.append(elem);
 }
 
